9e24002b8919e5125f8f54e9bbc02624300435c4
liam <liamkenna@live.com.au>
2021-12-14 21:12:48 +1100

    Day 9 Part 2 Saving Progress before trying new idea

--- Day 9.5/main.rs
+++ Day 9.5/main.rs
@@ -13,20 +13,12 @@ fn main() -> Result<(), Error> {
     let mut input: Vec<u16> = Vec::new();
     let width = dirty_input[0].len();
     let height = dirty_input.len();
-
     let mut risk = 0;
 
     for i in 0..dirty_input.len() {
-        //let bytes = dirty_input[i].as_bytes();
-        //let split_symbols: Vec<char> = dirty_input[i].chars().collect();
-
         for passed_char in dirty_input[i].chars(){
             input.push(passed_char.to_digit(10).unwrap() as u16);
-            //print!("{} ", input[0]);
         }
-        //print!("\n");
-        //println!("{}", split_symbols[0]);
-
     }
 
     for i in 0..input.len(){
@@ -35,54 +27,45 @@ fn main() -> Result<(), Error> {
                 if input[i] < input[i + width]
                 && input[i] < input[i + 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }
             else if (i + 1) % width == 0{ //right
                 if input[i] < input[i + width]
                 && input[i] < input[i - 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }else{//middle
                 if input[i] < input[i + width]
                 && input[i] < input[i + 1] 
                 && input[i] < input[i - 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }
-            
         }
         else if i >= input.len() - width { //bot
             if i % width == 0{ //left
                 if input[i] < input[i - width]
                 && input[i] < input[i + 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }
             else if (i + 1) % width == 0{ //right
                 if input[i] < input[i - width]
                 && input[i] < input[i - 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }else{//middle
                 if input[i] < input[i - width]
                 && input[i] < input[i + 1] 
                 && input[i] < input[i - 1] {
                     risk += input[i] + 1;
-                    //print!("{}\n", input[i]);
                 }
             }
         }else if i % width == 0{ //left
-            //print!("{}\n", input[i]);
             if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i + 1] {
                 risk += input[i] + 1;
-                //print!("{}\n", input[i]);
             }
         }
         else if (i + 1) % width == 0{ //right
@@ -91,21 +74,15 @@ fn main() -> Result<(), Error> {
                 && input[i] < input[i - width]
                 && input[i] < input[i - 1] {
                 risk += input[i] + 1;
-                //print!("{}\n", input[i]);
             }
         }else{ //middle
             if input[i] < input[i + width]
-            && input[i] < input[i - width]
-            && input[i] < input[i + 1] 
-            && input[i] < input[i - 1] {
-            risk += input[i] + 1;
-            //print!("{}\n", input[i]);
+                && input[i] < input[i - width]
+                && input[i] < input[i + 1] 
+                && input[i] < input[i - 1] {
+                risk += input[i] + 1;
+            }
         }
-        }
-    }
-
-    for i in 0..input.len() {
-        //if i <
     }
 
     println!("COUNT: {}", risk);
