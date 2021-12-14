c4a844dd7423bacaa3c64ec7ceba81be3b19a3ab
liam <liamkenna@live.com.au>
2021-12-14 21:23:01 +1100

    Day 9 Part 2 Complete

--- Day 9.5/main.rs
+++ Day 9.5/main.rs
@@ -8,83 +8,181 @@ fn read<R: Read>(io: R) -> Result<Vec<String>, Error> {
         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
         .collect()
 }
+
+//depth first search, does not work, pretty sure breadth would though.
+fn check_surrounding(input: &mut Vec<u32>, checked: &mut Vec<bool>, pos: usize, width: usize) -> u32{
+    let mut count = 1;
+    checked[pos] = true;
+
+    if pos < width { //top
+        if pos % width == 0{ //left
+            if !checked[pos + 1] && input[pos + 1] != 9{
+                count += check_surrounding(input, checked, pos + 1, width);
+            }
+            if !checked[pos + width] && input[pos + width] != 9{
+                count += check_surrounding(input, checked, pos + width, width);
+            }
+        }else if (pos + 1) % width == 0{ //right
+            if !checked[pos - 1] && input[pos - 1] != 9{
+                count += check_surrounding(input, checked, pos - 1, width);
+            }
+            if !checked[pos + width] && input[pos + width] != 9{
+                count += check_surrounding(input, checked, pos + width, width);
+            }
+        }else{//middle
+            if !checked[pos + 1] && input[pos + 1] != 9{
+                count += check_surrounding(input, checked, pos + 1, width);
+            }
+            if !checked[pos - 1] && input[pos - 1] != 9{
+                count += check_surrounding(input, checked, pos - 1, width);
+            }
+            if !checked[pos + width] && input[pos + width] != 9{
+                count += check_surrounding(input, checked, pos + width, width);
+            }
+        }
+    }else if pos >= input.len() - width { //bot
+        if pos % width == 0{ //left
+            if !checked[pos + 1] && input[pos + 1] != 9{
+                count += check_surrounding(input, checked, pos + 1, width);
+            }
+            if !checked[pos - width] && input[pos - width] != 9{
+                count += check_surrounding(input, checked, pos - width, width);
+            }
+        }else if (pos + 1) % width == 0{ //right
+            if !checked[pos - 1] && input[pos - 1] != 9{
+                count += check_surrounding(input, checked, pos - 1, width);
+            }
+            if !checked[pos - width] && input[pos - width] != 9{
+                count += check_surrounding(input, checked, pos - width, width);
+            }
+        }else{//middle
+            if !checked[pos + 1] && input[pos + 1] != 9{
+                count += check_surrounding(input, checked, pos + 1, width);
+            }
+            if !checked[pos - 1] && input[pos - 1] != 9{
+                count += check_surrounding(input, checked, pos - 1, width);
+            }
+            if !checked[pos - width] && input[pos - width] != 9{
+                count += check_surrounding(input, checked, pos - width, width);
+            }
+        }
+    }else if pos % width == 0{ //left
+        if !checked[pos + 1] && input[pos + 1] != 9{
+            count += check_surrounding(input, checked, pos + 1, width);
+        }
+        if !checked[pos + width] && input[pos + width] != 9{
+            count += check_surrounding(input, checked, pos + width, width);
+        }
+        if !checked[pos - width] && input[pos - width] != 9{
+            count += check_surrounding(input, checked, pos - width, width);
+        }
+    }else if (pos + 1) % width == 0{ //right
+        if !checked[pos - 1] && input[pos - 1] != 9{
+            count += check_surrounding(input, checked, pos - 1, width);
+        }
+        if !checked[pos + width] && input[pos + width] != 9{
+            count += check_surrounding(input, checked, pos + width, width);
+        }
+        if !checked[pos - width] && input[pos - width] != 9{
+            count += check_surrounding(input, checked, pos - width, width);
+        }
+    }else{ //middle
+        if !checked[pos - 1] && input[pos - 1] != 9{
+            count += check_surrounding(input, checked, pos - 1, width);
+        }
+        if !checked[pos + 1] && input[pos + 1] != 9{
+            count += check_surrounding(input, checked, pos + 1, width);
+        }
+        if !checked[pos + width] && input[pos + width] != 9{
+            count += check_surrounding(input, checked, pos + width, width);
+        }
+        if !checked[pos - width] && input[pos - width] != 9{
+            count += check_surrounding(input, checked, pos - width, width);
+        }
+
+    }
+
+    return count;
+}
+
+
 fn main() -> Result<(), Error> {
     let dirty_input = read(File::open("./input.txt")?)?;
-    let mut input: Vec<u16> = Vec::new();
+    let mut input: Vec<u32> = Vec::new();
+    let mut counts: Vec<u32> = Vec::new();
+    let mut checked: Vec<bool> = Vec::new();
     let width = dirty_input[0].len();
-    let height = dirty_input.len();
-    let mut risk = 0;
 
     for i in 0..dirty_input.len() {
         for passed_char in dirty_input[i].chars(){
-            input.push(passed_char.to_digit(10).unwrap() as u16);
+            input.push(passed_char.to_digit(10).unwrap() as u32);
+            checked.push(false);
         }
     }
 
     for i in 0..input.len(){
-        if i < width { //top
-            if i % width == 0{ //left
+        if !checked[i] {
+            if i < width { //top
+                if i % width == 0{ //left
+                    if input[i] < input[i + width]
+                    && input[i] < input[i + 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }else if (i + 1) % width == 0{ //right
+                    if input[i] < input[i + width]
+                    && input[i] < input[i - 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }else{//middle
+                    if input[i] < input[i + width]
+                    && input[i] < input[i + 1] 
+                    && input[i] < input[i - 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }
+            }else if i >= input.len() - width { //bot
+                if i % width == 0{ //left
+                    if input[i] < input[i - width]
+                    && input[i] < input[i + 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }else if (i + 1) % width == 0{ //right
+                    if input[i] < input[i - width]
+                    && input[i] < input[i - 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }else{//middle
+                    if input[i] < input[i - width]
+                    && input[i] < input[i + 1] 
+                    && input[i] < input[i - 1] {
+                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                    }
+                }
+            }else if i % width == 0{ //left
                 if input[i] < input[i + width]
-                && input[i] < input[i + 1] {
-                    risk += input[i] + 1;
-                }
-            }
-            else if (i + 1) % width == 0{ //right
-                if input[i] < input[i + width]
-                && input[i] < input[i - 1] {
-                    risk += input[i] + 1;
-                }
-            }else{//middle
-                if input[i] < input[i + width]
-                && input[i] < input[i + 1] 
-                && input[i] < input[i - 1] {
-                    risk += input[i] + 1;
-                }
-            }
-        }
-        else if i >= input.len() - width { //bot
-            if i % width == 0{ //left
-                if input[i] < input[i - width]
-                && input[i] < input[i + 1] {
-                    risk += input[i] + 1;
-                }
-            }
-            else if (i + 1) % width == 0{ //right
-                if input[i] < input[i - width]
-                && input[i] < input[i - 1] {
-                    risk += input[i] + 1;
-                }
-            }else{//middle
-                if input[i] < input[i - width]
-                && input[i] < input[i + 1] 
-                && input[i] < input[i - 1] {
-                    risk += input[i] + 1;
-                }
-            }
-        }else if i % width == 0{ //left
-            if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i + 1] {
-                risk += input[i] + 1;
-            }
-        }
-        else if (i + 1) % width == 0{ //right
-            //print!("{}\n", input[i]);
-            if input[i] < input[i + width]
+                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                }
+            }else if (i + 1) % width == 0{ //right
+                if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i - 1] {
-                risk += input[i] + 1;
-            }
-        }else{ //middle
-            if input[i] < input[i + width]
+                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                }
+            }else{ //middle
+                if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i + 1] 
                 && input[i] < input[i - 1] {
-                risk += input[i] + 1;
+                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
+                }
             }
         }
     }
 
-    println!("COUNT: {}", risk);
+    counts.sort();
+
+    println!("COUNT: {}", counts[counts.len() - 1] * counts[counts.len() - 2] * counts[counts.len() - 3]);
     Ok(())
 }
--- Day 9/main.rs
+++ Day 9/main.rs
@@ -8,267 +8,83 @@ fn read<R: Read>(io: R) -> Result<Vec<String>, Error> {
         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
         .collect()
 }
-
-//depth first search, does not work, pretty sure breadth would though.
-fn check_surrounding(input: &mut Vec<u16>, checked: &mut Vec<bool>, pos: usize, width: usize) -> u16{
-    let mut count = 0;
-    checked[pos] = true;
-    
-    //counts.push(check_surrounding(input, checked, i, width));
-    if pos < width { //top
-        if pos % width == 0{ //left
-            if (input[pos] < input[pos + width] || checked[pos + width])
-            && (input[pos] < input[pos + 1]  || checked[pos + 1]) {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos + 1]{
-                    count += check_surrounding(input, checked, pos + 1, width);
-                }
-                if !checked[pos + width]{
-                    count += check_surrounding(input, checked, pos + width, width);
-                }
-            }
-        }else if (pos + 1) % width == 0{ //right
-            if (input[pos] < input[pos + width]  || checked[pos + width])
-            && (input[pos] < input[pos - 1]  || checked[pos - 1]) {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos + width]{
-                    count += check_surrounding(input, checked, pos + width, width);
-                }
-                if !checked[pos - 1]{
-                    count += check_surrounding(input, checked, pos - 1, width);
-                }
-            }
-        }else{//middle
-            if (input[pos] < input[pos + width]  || checked[pos + width])
-            && (input[pos] < input[pos + 1]  || checked[pos + 1]) 
-            && (input[pos] < input[pos - 1]  || checked[pos - 1]) {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos + width]{
-                    count += check_surrounding(input, checked, pos + width, width);
-                }
-                if !checked[pos + 1]{
-                    count += check_surrounding(input, checked, pos + 1, width);
-                }
-                if !checked[pos - 1]{
-                    count += check_surrounding(input, checked, pos - 1, width);
-                }
-            }
-        }
-    }else if pos >= input.len() - width { //bot
-        if pos % width == 0{ //left
-            if (input[pos] < input[pos - width]  || checked[pos - width])
-            && (input[pos] < input[pos + 1]  || checked[pos + 1]) {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos - width]{
-                    count += check_surrounding(input, checked, pos - width, width);
-                }
-                if !checked[pos + 1]{
-                    count += check_surrounding(input, checked, pos + 1, width);
-                }
-            }
-        }else if (pos + 1) % width == 0{ //right
-            if (input[pos] < input[pos - width] || checked[pos - width]) 
-            && (input[pos] < input[pos - 1] || checked[pos - 1])  {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos - width]{
-                    count += check_surrounding(input, checked, pos - width, width);
-                }
-                if !checked[pos - 1]{
-                    count += check_surrounding(input, checked, pos - 1, width);
-                }
-            }
-        }else{//middle
-            if (input[pos] < input[pos - width] || checked[pos - width]) 
-            && (input[pos] < input[pos + 1] || checked[pos + 1]) 
-            && (input[pos] < input[pos - 1] || checked[pos - 1])  {
-                //risk += input[pos] + 1;
-                count += 1;
-                if !checked[pos - width]{
-                    count += check_surrounding(input, checked, pos - width, width);
-                }
-                if !checked[pos + 1]{
-                    count += check_surrounding(input, checked, pos + 1, width);
-                }
-                if !checked[pos - 1]{
-                    count += check_surrounding(input, checked, pos - 1, width);
-                }
-            }
-        }
-    }else if pos % width == 0{ //left
-        if (input[pos] < input[pos + width] || checked[pos + width]) 
-        && (input[pos] < input[pos - width] || checked[pos - width]) 
-        && (input[pos] < input[pos + 1] || checked[pos + 1])  {
-            //risk += input[pos] + 1;
-            count += 1;
-            if !checked[pos + width]{
-                count += check_surrounding(input, checked, pos + width, width);
-            }
-            if !checked[pos - width]{
-                count += check_surrounding(input, checked, pos - width, width);
-            }
-            if !checked[pos + 1]{
-                count += check_surrounding(input, checked, pos + 1, width);
-            }
-        }
-    }else if (pos + 1) % width == 0{ //right
-        if (input[pos] < input[pos + width] || checked[pos + width]) 
-        && (input[pos] < input[pos - width] || checked[pos - width]) 
-        && (input[pos] < input[pos - 1] || checked[pos - 1])  {
-            //risk += input[pos] + 1;
-            count += 1;
-            if !checked[pos + width]{
-                count += check_surrounding(input, checked, pos + width, width);
-            }
-            if !checked[pos - width]{
-                count += check_surrounding(input, checked, pos - width, width);
-            }
-            if !checked[pos - 1]{
-                count += check_surrounding(input, checked, pos - 1, width);
-            }
-        }
-    }else{ //middle
-        if (input[pos] < input[pos + width] || checked[pos + width]) 
-        && (input[pos] < input[pos - width] || checked[pos - width]) 
-        && (input[pos] < input[pos + 1] || checked[pos + 1])  
-        && (input[pos] < input[pos - 1] || checked[pos - 1]) {
-            //risk += input[pos] + 1;
-            count += 1;
-            if !checked[pos + width]{
-                count += check_surrounding(input, checked, pos + width, width);
-            }
-            if !checked[pos - width]{
-                count += check_surrounding(input, checked, pos - width, width);
-            }
-            if !checked[pos + 1]{
-                count += check_surrounding(input, checked, pos + 1, width);
-            }
-            if !checked[pos - 1]{
-                count += check_surrounding(input, checked, pos - 1, width);
-            }
-        }
-    }
-
-    return count;
-}
-
-
 fn main() -> Result<(), Error> {
     let dirty_input = read(File::open("./input.txt")?)?;
     let mut input: Vec<u16> = Vec::new();
-    let mut counts: Vec<u16> = Vec::new();
-    let mut checked: Vec<bool> = Vec::new();
     let width = dirty_input[0].len();
+    let height = dirty_input.len();
     let mut risk = 0;
 
     for i in 0..dirty_input.len() {
         for passed_char in dirty_input[i].chars(){
             input.push(passed_char.to_digit(10).unwrap() as u16);
-            checked.push(false);
         }
     }
 
     for i in 0..input.len(){
-        if !checked[i] {
-            if i < width { //top
-                if i % width == 0{ //left
-                    if input[i] < input[i + width]
-                    && input[i] < input[i + 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }else if (i + 1) % width == 0{ //right
-                    if input[i] < input[i + width]
-                    && input[i] < input[i - 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }else{//middle
-                    if input[i] < input[i + width]
-                    && input[i] < input[i + 1] 
-                    && input[i] < input[i - 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }
-            }else if i >= input.len() - width { //bot
-                if i % width == 0{ //left
-                    if input[i] < input[i - width]
-                    && input[i] < input[i + 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }else if (i + 1) % width == 0{ //right
-                    if input[i] < input[i - width]
-                    && input[i] < input[i - 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }else{//middle
-                    if input[i] < input[i - width]
-                    && input[i] < input[i + 1] 
-                    && input[i] < input[i - 1] {
-                        //risk += input[i] + 1;
-                        //checked[i] = true;
-                        counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                    }
-                }
-            }else if i % width == 0{ //left
+        if i < width { //top
+            if i % width == 0{ //left
                 if input[i] < input[i + width]
+                && input[i] < input[i + 1] {
+                    risk += input[i] + 1;
+                }
+            }
+            else if (i + 1) % width == 0{ //right
+                if input[i] < input[i + width]
+                && input[i] < input[i - 1] {
+                    risk += input[i] + 1;
+                }
+            }else{//middle
+                if input[i] < input[i + width]
+                && input[i] < input[i + 1] 
+                && input[i] < input[i - 1] {
+                    risk += input[i] + 1;
+                }
+            }
+        }
+        else if i >= input.len() - width { //bot
+            if i % width == 0{ //left
+                if input[i] < input[i - width]
+                && input[i] < input[i + 1] {
+                    risk += input[i] + 1;
+                }
+            }
+            else if (i + 1) % width == 0{ //right
+                if input[i] < input[i - width]
+                && input[i] < input[i - 1] {
+                    risk += input[i] + 1;
+                }
+            }else{//middle
+                if input[i] < input[i - width]
+                && input[i] < input[i + 1] 
+                && input[i] < input[i - 1] {
+                    risk += input[i] + 1;
+                }
+            }
+        }else if i % width == 0{ //left
+            if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i + 1] {
-                    //risk += input[i] + 1;
-                    //checked[i] = true;
-                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                }
-            }else if (i + 1) % width == 0{ //right
-                if input[i] < input[i + width]
+                risk += input[i] + 1;
+            }
+        }
+        else if (i + 1) % width == 0{ //right
+            //print!("{}\n", input[i]);
+            if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i - 1] {
-                    //risk += input[i] + 1;
-                    //checked[i] = true;
-                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                }
-            }else{ //middle
-                if input[i] < input[i + width]
+                risk += input[i] + 1;
+            }
+        }else{ //middle
+            if input[i] < input[i + width]
                 && input[i] < input[i - width]
                 && input[i] < input[i + 1] 
                 && input[i] < input[i - 1] {
-                    //risk += input[i] + 1;
-                    //checked[i] = true;
-                    counts.push(check_surrounding(&mut input, &mut checked, i, width));
-                }
+                risk += input[i] + 1;
             }
         }
     }
 
-    for i in 0..input.len(){
-        if checked[i]{
-            print!("[{}] ", input[i]);
-        }else{
-            print!(" {}  ", input[i]);
-        }
-
-        if (i + 1) % width == 0 {
-            print!("\n");
-        }
-    }
-
-    counts.sort();
-    print!("\n");print!("\n");
-    for i in 0..counts.len() {
-        println!("{} \n", counts[i]);
-    }
-
-    println!("COUNT: {}", counts[counts.len() - 1] * counts[counts.len() - 2] * counts[counts.len() - 3]);
+    println!("COUNT: {}", risk);
     Ok(())
 }
